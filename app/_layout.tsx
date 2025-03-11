6ad574cc5bb8ceabc7c0c3f987890037bf9bfc28
KMalkowski <krzysztof.malkowski098@gmail.com>
2025-03-11 11:04:25 +0100

    carplay connected to app init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { CarPlay } from "react-native-carplay";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -14,7 +19,28 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  const [carPlayConnected, setCarPlayConnected] = useState(CarPlay.connected);
+  console.log("carPlayConnected", carPlayConnected);
+
+  useEffect(() => {
+    function onConnect() {
+      setCarPlayConnected(true);
+    }
+
+    function onDisconnect() {
+      setCarPlayConnected(false);
+    }
+
+    CarPlay.registerOnConnect(onConnect);
+    CarPlay.registerOnDisconnect(onDisconnect);
+
+    return () => {
+      CarPlay.unregisterOnConnect(onConnect);
+      CarPlay.unregisterOnDisconnect(onDisconnect);
+    };
   });
 
   useEffect(() => {
@@ -28,7 +54,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
